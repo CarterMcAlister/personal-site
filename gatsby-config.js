effca3b49f39450256a605e244ba2ff5b7805b9a
Carter <carter@rocketmiles.com>
2020-01-10 18:19:55 -0800

    Update branding and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter w/ styled-components`,
-    description: `Gatsby starter using styled-components`,
-    author: `@blakenoll`
+    title: `Carter McAlister`,
+    description: `Personal Site`,
+    author: `Carter McAlister`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/cm.png` // This path is relative to the root of the site.
       }
     },
     `gatsby-plugin-styled-components`,
