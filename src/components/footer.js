bb35a14fda31d0161f4ecd4a4a32b41338d9e048
Carter <5679378+CarterMcAlister@users.noreply.github.com>
2022-02-02 00:59:34 -0600

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,16 +7,6 @@ const Footer = () => (
   <StyledFooter>
     <Container>
       <Text>© {new Date().getFullYear()} - Carter McAlister</Text>
-      <Text>
-        Built with{" "}
-        <a
-          href="https://www.gatsbyjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Gatsby
-        </a>
-      </Text>
     </Container>
   </StyledFooter>
 );
